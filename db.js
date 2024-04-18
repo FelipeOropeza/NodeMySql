971ea2ef749e21c41dcfecb13d28bf58a35bb4a4
FelipeOropeza <felipe.oropeza@etec.sp.gov.br>
2024-04-18 01:39:49 -0300

    chore: crud completo

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ import mysql from "mysql2/promise";
 
 async function connect() {
   const connection = await mysql.createConnection(
-    "mysql://root:senac@localhost:3307/nodeJsMySql"
+    "mysql://root:kiraFE22022006@localhost:3306/teste"
   );
   global.connection = connection;
   return connection;
@@ -10,17 +10,66 @@ async function connect() {
 
 async function selectProdutos() {
   const conn = await connect();
-  const [rows] = await conn.query("SELECT * FROM produtos;");
-  return rows;
+
+  try {
+    const [rows] = await conn.query("SELECT * FROM testeProd;");
+    conn.end();
+    return rows.map((row) => ({
+      id_prod: row.id_prod,
+      nome: row.nome,
+      valor: row.valor,
+    }));
+  } catch (error) {
+    conn.end();
+    return "Error ao selecionar produtos";
+  }
 }
 
 async function insertProduto(insert) {
   const conn = await connect();
-  const partes = insert.split(", ");
-  const sql = "INSERT INTO produtos VALUES (?,?,?);";
-  const values = [, partes[0], partes[1]];
-  await conn.query(sql, values);
-  return "Insert feito!";
+
+  try {
+    const partes = insert.split(", ");
+    const sql = "INSERT INTO testeProd VALUES (?,?,?);";
+    const values = [, partes[0], Number(partes[1])];
+    await conn.query(sql, values);
+    conn.end();
+    return "Insert feito!";
+  } catch (error) {
+    conn.end();
+    return "Error ao inserir produto";
+  }
 }
 
-export { selectProdutos, insertProduto};
+async function updateProduto(update) {
+  const conn = await connect();
+
+  try {
+    const partes = update.split(", ");
+    const sql = "UPDATE testeProd set nome = ?, valor = ? where id_prod = ?;";
+    const values = [partes[1], Number(partes[2]), Number(partes[0])];
+    await conn.query(sql, values);
+    conn.end();
+    return "Atualização feita!";
+  } catch (error) {
+    conn.end();
+    return "Error ao atualizar produto";
+  }
+}
+
+async function deleteProduto(id) {
+  const conn = await connect();
+
+  try {
+    const sql = "DELETE from testeProd where id_prod = ?;";
+    const values = [Number(id)];
+    await conn.query(sql, values);
+    conn.end();
+    return "Deleção feita!";
+  } catch (error) {
+    conn.end();
+    return "Error ao deletar produto";
+  }
+}
+
+export { selectProdutos, insertProduto, updateProduto, deleteProduto };
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { selectProdutos, insertProduto } from "./db.js";
+import { selectProdutos, insertProduto, updateProduto, deleteProduto } from "./db.js";
 import readline from "readline";
 
 const rl = readline.createInterface({
@@ -7,21 +7,44 @@ const rl = readline.createInterface({
 });
 
 rl.question(
-  "1 - Selecionar produtos \n2 - Cadastrar produto \n3 - Deletar produto \n\nSeleciona uma função: ",
+  "1 - Selecionar produtos \n2 - Cadastrar produto \n3 - Atualizar produto \n4 - Deletar produto \n\nSeleciona uma função: ",
   async (valor) => {
     switch (valor) {
       case "1":
-        console.log(await selectProdutos());
+        console.log("Selecionando produtos...");
+        const produtos = await selectProdutos();
+        console.log(produtos);
+        rl.close();
         break;
       case "2":
-        rl.question("Digite o nome e produto: ", async (insert) => {
+        rl.question("Digite o nome e o valor do produto: ", async (insert) => {
           console.log(await insertProduto(insert));
+          rl.close();
         });
         break;
       case "3":
+        console.log(await selectProdutos());
+        rl.question(
+          "Digite o id e as informaçoes a serem atualizadas: ",
+          async (update) => {
+            console.log(await updateProduto(update));
+            rl.close();
+          }
+        );
+        break;
+      case "4":
+        console.log(await selectProdutos());
+        rl.question(
+          "Digite o id do produto: ",
+          async (id) => {
+            console.log(await deleteProduto(id));
+            rl.close();
+          }
+        );
         break;
       default:
         console.log("Função não existe!!");
+        rl.close();
     }
   }
 );
